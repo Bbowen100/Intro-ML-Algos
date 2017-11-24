3c8cb59c46bc884ebb3dc4e56b976f15bd7b64ee
Brandon Bowen <brandonklbowen@gmail.com>
2017-11-24 00:40:02 -0500

    refactored kmeans

--- kmeans.m
+++ kmeans.m
@@ -8,6 +8,7 @@ if (isempty(cent_init))
   % Initial centers is an empty array, choose initial centers
   % randomly
 
+  % picks k random numbers from 1 to number of data points (for indices)
   entries_i=randperm(size(data,1),k);
   % takes row vectors from data at indices specified above
   centers = data(entries_i,:);
@@ -21,40 +22,32 @@ else
 end;
 
 % REMINDER variable labels is defined above, also CODE STARTS HERE
-perm_labels= zeros(size(data,1),k); % hold the permanent label matrix
 while 1 % until the labeling converges
   temp_labels = zeros(size(data,1),k); % hold the temporary label matrix
   n_centers = []; % hold the temporary centers
-  for row1 = 1:size(data,1) % assign labels to data points
-    x_i = data(row1,:);
-    % find the center with the smallest distance
-    d_i = sqrt(sum((x_i-centers).^2,2));
-    [min_d, min_vec_idx] = min(d_i);
-    % make val @ row, min_vec_idx in temp_labels = 1
-    temp_labels(row1, min_vec_idx) = 1;
-  end
+  rcenters = reshape(centers',[1, size(data,2), k]);
+  rdata = repmat(data, [1,1,k]);
+  dif = rdata - rcenters;
+  dist = sqrt(sum(dif.^2,2));
+  dist = reshape(dist, size(data,1),k);
+  [m,i] = min(dist');
+  temp_labels = i';
 
   % check if the labeling has changed
-  if (isequal(temp_labels,perm_labels))
+  if (isequal(temp_labels,labels))
     break;
   else
-    perm_labels = temp_labels;
+    labels = temp_labels;
   end;
 
   % move the centers around
-  for row2 = 1:size(centers,1) % 1 to k
-    c_i = perm_labels(:,row2); % take column vector c_i from perm_labels
-    % set all data points not labeled by c_i to 0 and sum all of the remaining
-    S_i = sum(data.*c_i);
-    D_i = sum(c_i); % get number of data points labeled by c_i
-    avg_center = S_i/D_i; % get the average
+  for center = 1:k % 1 to k
+    rel_data = data(labels==center,:);
+    rdSize = size(rel_data,1);
+
+    avg_center = sum(rel_data) ./ rdSize; % get the average
     n_centers = [n_centers; avg_center]; % append the center to matrix
-  end
+  end;
   % set new centers to old centers
   centers = n_centers;
-end
-% compress perm_labels to labels
-for row3 = 1:size(perm_labels,1); % each row in perm_labels
-  [c, c_max_idx] = max(perm_labels(row3,:));
-  labels(row3, :) = c_max_idx;
-end
+end;
